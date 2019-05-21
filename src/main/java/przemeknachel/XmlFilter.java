aef2d6db71e07623eca1bef1e9fb4a14d8d55203
przemekNachel <przemekNachel@gmail.com>
2019-05-21 13:58:05 +0200

    Add tags

--- src/main/java/przemeknachel/XmlFilter.java
+++ src/main/java/przemeknachel/XmlFilter.java
@@ -17,6 +17,15 @@ public class XmlFilter {
     private final Node doc;
     private String stringToCheck;
 
+    public static void main(String[] args) {
+        System.out.println("1. " + new XmlFilter("filter.xml").check("8=FIX.4.4|9=395|35=8|49=SENDER|56=TARGET|52=20160101-10:16:11.877|122=20160101-10:16:11|34=15425|115=BAPP|17=12345678-1|527=321765860354|150=F|31=16.00|32=166|30=DSMD|29=1|54=1|10006=1|60=20160101-10:16:11.000|382=1|375=AERT|75=20160101|775=0|119=2686.00|55=N/A|14=0|6=0|528=A|22=100|48=0003012345|10=228|"));
+        System.out.println("2. " + new XmlFilter("filter.xml").check("8=FIX.4.4|9=391|35=8|49=SENDER|56=TARGET|52=20160101-10:50:59.929|122=20160101-10:50:59|34=15697|115=BAPP|17=98765421-1|527=000127251481|150=F|31=250.00|32=400|30=XOTC|29=5|54=1|10006=1|60=20160101-10:50:58.000|382=1|375=12345|75=20160101|775=0|119=100000.00|55=N/A|14=0|6=0|528=R|22=100|48=0002912345|10=153|"));
+        System.out.println("3. " + new XmlFilter("filter.xml").check("8=FIX.4.4|9=388|35=8|49=SENDER|56=TARGET|52=20160101-13:31:17.181|122=20160101-13:31:17|34=29430|115=BAPP|17=54789787-1|527=000021454874|150=F|31=250.00|32=92|30=XYZX|29=5|54=1|10006=1|60=20160101-13:31:14.000|382=1|375=12345|75=20160101|775=0|119=23000.00|55=N/A|14=0|6=0|528=R|22=100|48=0002912345|10=036|"));
+        System.out.println("4. " + new XmlFilter("filter.xml").check("8=FIX.4.4|9=395|35=8|49=SENDER|56=TARGET|52=20160101-13:17:27.556|122=20160101-13:17:27|34=29374|115=BAPP|17=216546544-1|527=37218187454213|150=F|31=42.00|32=110|30=DSMD|29=1|54=1|10006=1|60=20160101-13:17:27.000|382=1|375=AERT|75=20160101|775=0|119=4650.00|55=N/A|14=0|6=0|528=A|22=100|48=0003012345|10=052|"));
+        System.out.println("5. " + new XmlFilter("filter.xml").check("8=FIX.4.4|9=378|35=8|49=SENDER|56=TARGET|52=20160101-10:31:13.872|122=20160101-10:31:13|34=18926|115=ASDF|17=32165478-1|527=RTE321654654|150=F|31=0.85|32=3|30=XXXX|29=4|54=2|10006=1|60=20160101-10:31:12.000|375=10098|75=20160101|775=0|119=2.55|55=N/A|14=0|6=0|528=A|22=100|48=123456789|10=166|"));
+        System.out.println("6. " + new XmlFilter("filter.xml").check("8=FIX.4.4|9=348|35=8|49=SENDER|56=TARGET|52=20160101-10:31:14.035|122=20160101-10:31:14|34=18927|115=HW-ASDF1|17=456123485-1|527=TYU321655454|150=F|31=0.85|32=3|30=XXYY|29=4|54=1|10006=1|60=20160101-10:31:12.000|382=1|375=1238|75=20160101|775=E|55=N/A|14=0|6=0|528=Y|22=100|48=12456789|10=140|"));
+    }
+
     public XmlFilter(String filePath) {
         doc = getDocumentNode(filePath).getChildNodes().item(0);
     }
@@ -95,13 +104,14 @@ public class XmlFilter {
         String type = node.getAttributes().getNamedItem("type").getTextContent();
         String field = node.getAttributes().getNamedItem("field").getTextContent();
         String value = node.getAttributes().getNamedItem("value").getTextContent();
+        System.out.println("type: " + type + " field: " + field + " value: " + value);
         switch (type) {
             case "equals":
                 return getValue(field).equals(value);
             case "in":
-                break;
+                return Arrays.asList(value.split(",")).contains(getValue(field));
             case "matches":
-                break;
+                return getValue(field).matches(value);
         }
         return true;
     }
